31ae14d8da11fba19aae5290a7767e48a5221aa7
sunxr0119 <563853814@qq.com>
2019-03-07 15:20:48 +0800

    ex3

---
+++ python_hard/ex3.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+def _odd_iter():
+    n=1
+    while True:
+        n=n+2
+        yield n
+
+def _not_divisible(n):
+    return lambda x:x%n>0
+
+def primes():
+    yield 2
+    it=_odd_iter()#初始序列
+    while True:
+        n=next(it)#返回序列第一个值
+        yield n
+        it=filter(_not_divisible(n),it)#更新序列
+
+# for n in primes():
+#     if n < 1000:
+#         print(n)
+#     else:
+#         break
+
